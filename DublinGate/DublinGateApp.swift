6bb2ea266981201f73fb5d95d42d5c21a7eb763a
Horacio Mota <horaciomota93u@gmail.com>
2023-10-16 10:08:51 -0700

    Initial Commit

---
+++ DublinGate/DublinGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DublinGateApp.swift
+//  DublinGate
+//
+//  Created by Horacio Mota on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DublinGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
